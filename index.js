da86b76a0c4dfc3211f758ca93295b3a5a86f4a2
azzy kesuma <official.azzy@gmail.com>
2021-11-30 08:21:09 +0700

    styling index

--- index.js
+++ index.js
@@ -24,5 +24,21 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  // dots[slideIndex-1].className += " active";
+}
+// hover state in work section
+const webDev = document.getElementById('web-development');
+const workDescription = document.querySelector('.workDescription');
+console.log(workDescription);
+// make hover function
+webDev.addEventListener('mouseenter', mouse_enter);
+webDev.addEventListener('mouseleave', mouse_leave);
+
+function mouse_enter() {
+  // webDev.style.opacity = '.6';
+  workDescription.classList.add('work_active')
+}
+function mouse_leave() {
+  // webDev.style.opacity = '1';
+  workDescription.classList.remove('work_active')
 }
